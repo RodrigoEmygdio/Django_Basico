4cb4e77ded654f5050bbd4bdaa3c952063da52d8
Rodrigo Emygdio <rodrigo.emygdio@gmail.com>
2020-04-10 11:32:31 -0300

    Adicionado arquivos estaticos ao projeto

--- Django_Basico/settings.py
+++ Django_Basico/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'e945j0vp!0&+*ree)v1*%dnuq1upm6br(atgm9ma_@#^_an#!4'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -55,7 +53,10 @@ ROOT_URLCONF = 'Django_Basico.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR), 'receitas/templates'
+        ]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
@@ -71,7 +72,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Django_Basico.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
@@ -81,7 +81,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -101,7 +100,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https ://docs.djangoproject.com/en/3.0/topics/i18n/
 
@@ -115,8 +113,11 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'receitas/static')
+]
